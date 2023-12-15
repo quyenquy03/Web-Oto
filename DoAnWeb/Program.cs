1bd96e2ba18e2cbd58bd9d98fdbae3311d42a9cb
quyenquy03 <ta2k3quyen@gmail.com>
2023-12-15 22:53:50 +0700

    hello
    update

--- DoAnWeb/Program.cs
+++ DoAnWeb/Program.cs
@@ -1,3 +1,5 @@
+﻿using AspNetCoreHero.ToastNotification;
+using AspNetCoreHero.ToastNotification.Extensions;
 using DoAnWeb.Context;
 using DoAnWeb.Models;
 using Microsoft.EntityFrameworkCore;
@@ -7,8 +9,17 @@ var builder = WebApplication.CreateBuilder(args);
 var connection = builder.Configuration.GetConnectionString("DefaultConnection");
 builder.Services.AddDbContext<MyDbContext>(options => options.UseSqlServer(connection));
 // Add services to the container.
-builder.Services.AddControllersWithViews();
+builder.Services.AddControllersWithViews().AddRazorRuntimeCompilation();
+builder.Services.AddSassCompiler();
+builder.Services.AddDistributedMemoryCache();
+builder.Services.AddSession(cfg => {                    // Đăng ký dịch vụ Session
+	cfg.Cookie.Name = "BonsaiShop";             // Đặt tên Session - tên này sử dụng ở Browser (Cookie)
+	cfg.IdleTimeout = new TimeSpan(0, 30, 0);    // Thời gian tồn tại của Session
+	cfg.Cookie.HttpOnly = true;
+	cfg.Cookie.IsEssential = true;
+});
 
+builder.Services.AddNotyf(config => { config.DurationInSeconds = 10; config.IsDismissable = true; config.Position = NotyfPosition.TopRight; });
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -21,10 +32,11 @@ if (!app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
-
+app.UseNotyf();
 app.UseRouting();
 
 app.UseAuthorization();
+app.UseSession();
 
 app.UseEndpoints(endpoints =>
 {
